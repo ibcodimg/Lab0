324c9e1dd34fd0c297578964cb881d13bb99c79c
Bill Ma <billma@Mac.lan>
2026-02-01 23:41:28 -0800

    Update App.java

--- CS2400/Lab0/Sum/src/App.java
+++ CS2400/Lab0/Sum/src/App.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
+
 public class App {
+
+    public static  int add(int a, int b) {
+            return a + b;
+        }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter the first number you wish to add: ");
+        int num1 = keyboard.nextInt();
+        System.out.println("Enter the second number you wish to add: ");
+        int num2 = keyboard.nextInt();
+
+        int sum = add(num1, num2);
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
 }
